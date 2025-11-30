9fcd1d42a68e0d928522b36fe4265f6897c046ee
Enise Sultan Kaya <109233701+enisesultankaya@users.noreply.github.com>
2025-11-30 03:31:50 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { HomePage } from './components/HomePage';
@@ -8,7 +8,7 @@ import { ProjectDetailPage } from './components/ProjectDetailPage';
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-1">
@@ -21,6 +21,6 @@ export default function App() {
         </main>
         <Footer />
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
